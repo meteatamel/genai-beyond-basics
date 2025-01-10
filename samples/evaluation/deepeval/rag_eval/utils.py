27b92dab2a852d4ae530e52307b692db964dce42
Mete Atamel <atamel@google.com>
2025-01-10 15:14:57 +0400

    Updated DeepEval samples

--- samples/evaluation/deepeval/rag_eval/utils.py
+++ samples/evaluation/deepeval/rag_eval/utils.py
@@ -8,17 +8,34 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_vertexai import VertexAIEmbeddings, ChatVertexAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-EMBEDDING_MODEL_NAME = "text-embedding-005"
-CHUNK_SIZE = 500
-CHUNK_OVERLAP = 100
 PDF_PATH = "cymbal-starlight-2024.pdf"
-SYSTEM_PROMPT = """You are an assistant for question-answering tasks. 
+
+EMBEDDING_MODEL_NAME_1 = "textembedding-gecko@003"
+EMBEDDING_MODEL_NAME_2 = "text-embedding-005"
+EMBEDDING_MODEL_NAME = EMBEDDING_MODEL_NAME_2
+
+CHUNK_SIZE_1 = 500
+CHUNK_SIZE_2 = 200
+CHUNK_SIZE = CHUNK_SIZE_2
+
+CHUNK_OVERLAP_1 = 100
+CHUNK_OVERLAP_2 = 0
+CHUNK_OVERLAP = CHUNK_OVERLAP_2
+
+SYSTEM_PROMPT_1 = """You are an assistant for question-answering tasks. 
 Use the following pieces of retrieved context to answer 
 the question. If you don't know the answer, say that you 
 don't know. Use three sentences maximum and keep the 
 answer concise.
 
 {context}"""
+SYSTEM_PROMPT_2 = """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer 
+the question. Only answer the question and if you don't know the answer, 
+just say I don't know.
+
+{context}"""
+SYSTEM_PROMPT = SYSTEM_PROMPT_2
 
 MODEL_NAME="gemini-1.5-flash-002"
 TEMPERATURE=1
@@ -46,7 +63,8 @@ def setup_rag_chain():
         embedding=embeddings_model,
     )
 
-    retriever = vector_store.as_retriever()
+    #retriever = vector_store.as_retriever()
+    retriever = vector_store.as_retriever(search_kwargs={"k": 1})
 
     print(f"Initialize the model: {MODEL_NAME}")
     model = ChatVertexAI(
