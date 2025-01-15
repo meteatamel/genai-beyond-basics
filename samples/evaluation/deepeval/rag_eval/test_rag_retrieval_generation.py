8e5f79e11c5ead766668b73beb53b1f20e6f5845
Mete Atamel <atamel@google.com>
2025-01-15 14:32:33 +0400

    Updated DeepEval samples

--- samples/evaluation/deepeval/rag_eval/test_rag_retrieval_generation.py
+++ samples/evaluation/deepeval/rag_eval/test_rag_retrieval_generation.py
@@ -12,22 +12,31 @@ from utils import get_project_id
 # RAG evaluation
 # https://docs.confident-ai.com/docs/guides-rag-evaluation
 
-EVAL_MODEL_NAME = "gemini-1.5-pro-002"
-
-def test_retrieval():
-    test_case = LLMTestCase(
+def get_test_case(expected_output=None):
+    return LLMTestCase(
         input="I'm on an F-1 visa, how long can I stay in the US after graduation?",
         actual_output="You can stay up to 30 days after completing your degree.",
-        expected_output="You can stay up to 60 days after completing your degree.",
+        expected_output=expected_output,
         retrieval_context=[
             """If you are in the U.S. on an F-1 visa, you are allowed to stay for 60 days after completing
             your degree, unless you have applied for and been approved to participate in OPT."""
         ]
     )
 
-    eval_model = GoogleVertexAI(model_name=EVAL_MODEL_NAME,
-                           project=get_project_id(),
-                           location="us-central1")
+EVAL_MODEL_NAME = "gemini-1.5-pro-002"
+
+def get_eval_model():
+    return GoogleVertexAI(
+        model_name=EVAL_MODEL_NAME,
+        project=get_project_id(),
+        location="us-central1"
+    )
+
+def test_retrieval():
+    test_case = get_test_case(
+        expected_output="You can stay up to 60 days after completing your degree."
+    )
+    eval_model = get_eval_model()
 
     metrics = [
         ContextualPrecisionMetric(model=eval_model),
@@ -39,18 +48,8 @@ def test_retrieval():
 
 
 def test_generation():
-    test_case = LLMTestCase(
-        input="I'm on an F-1 visa, how long can I stay in the US after graduation?",
-        actual_output="You can stay up to 30 days after completing your degree.",
-        retrieval_context=[
-            """If you are in the U.S. on an F-1 visa, you are allowed to stay for 60 days after completing
-            your degree, unless you have applied for and been approved to participate in OPT."""
-        ]
-    )
-
-    eval_model = GoogleVertexAI(model_name=EVAL_MODEL_NAME,
-                           project=get_project_id(),
-                           location="us-central1")
+    test_case = get_test_case()
+    eval_model = get_eval_model()
 
     metrics = [
         AnswerRelevancyMetric(model=eval_model),
