9140ed59d01d12b9680907188edc4e8c26780ad8
Mete Atamel <atamel@google.com>
2025-03-05 15:18:33 +0000

    Minor updates

--- samples/grounding/llamaindex-vertexai/main.py
+++ samples/grounding/llamaindex-vertexai/main.py
@@ -1,8 +1,11 @@
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core import Document
 from llama_index.core import VectorStoreIndex
+from llama_index.core import StorageContext, load_index_from_storage
+from llama_index.core.node_parser import SentenceSplitter
 from llama_index.llms.vertex import Vertex
 from llama_index.embeddings.vertex import VertexTextEmbedding
+
 import google.auth
 
 def main():
@@ -12,9 +15,6 @@ def main():
         input_files=["./cymbal-starlight-2024.pdf"]
     ).load_data()
 
-    print("Combine document for each page back into a single document")
-    document = Document(text="\n\n".join([doc.text for doc in documents]))
-
     print("Initialize embedding model")
     credentials, project_id = google.auth.default()
     embed_model = VertexTextEmbedding(
@@ -22,10 +22,22 @@ def main():
         model_name= "text-embedding-005",
     )
 
-    print("Index document")
+    # Optional: If you stored index before, you can simply reload it
+    # print("Reload index locally")
+    # storage_context = StorageContext.from_defaults(persist_dir="index")
+    # index = load_index_from_storage(storage_context, embed_model=embed_model)
+
+    # Optional: You can customize chunk_size and chunk_overlap
+    text_splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
+
+    print("Index documents")
     index = VectorStoreIndex.from_documents(
-        [document], embed_model=embed_model
+        documents, embed_model=embed_model, transformations=[text_splitter]
     )
+
+    # Optional: You can store the index locally
+    # print("Store index locally")
+    # index.storage_context.persist(persist_dir="index")
 
     print("Initialize the query engine with the model")
     llm = Vertex(model="gemini-2.0-flash-001")
